e97f55007285b1e09097fbad52be9e8b68408696
a-garcia96 <alex.g9415@gmail.com>
2022-09-15 12:56:46 -0700

    delete files. new files. modift files.

---
+++ components/Nav.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Link from 'next/Link'
+
+
+export default function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link href='/'>
+                        <a>Home</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/about'>
+                        <a>About</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/contact'>
+                        <a>Contact</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/projects'>
+                        <a>Projects</a>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
---
+++ pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <header>
+        <div className='container'>
+          
+        </div>
+        <h1>Alex Garica</h1>
+      </header>
+
+      <main className='container'>
+        
+      </main>
+        </div>
+    )
+}
