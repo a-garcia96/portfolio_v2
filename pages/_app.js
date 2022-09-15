2738e20bc90de9e8746e8255434dde72367422dd
a-garcia96 <alex.g9415@gmail.com>
2022-09-15 15:31:52 -0700

    add border-box sizing to root

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component className='box-border' {...pageProps} />
 }
 
 export default MyApp
