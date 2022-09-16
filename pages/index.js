fd6d0dff7286c66a33f30c4cb115fe3f15a0a4b3
a-garcia96 <alex.g9415@gmail.com>
2022-09-16 13:49:57 -0700

    work on header

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+// NEXTJS COMPONENTS
 import Head from 'next/head'
-
+import Image from 'next/image'
 // components
 import Nav from '../components/Nav'
+
+// assets
+import profilePic from '../public/profilePic.jpg'
 
 
 export default function Home() {
@@ -12,9 +16,25 @@ export default function Home() {
         <meta name="description" content="Built with NextJs and Tailwind" />
       </Head>
       <Nav />
-      <header>
-        <div className='container'>
-          <h1>Alex Garica</h1>
+      <header className='p-8'>
+        <div className='container mx-auto grid grid-cols-1 place-items-center'>
+          <div>
+          <Image
+              src={profilePic}
+              width={300}
+              height={300}
+              placeholder='blur'
+            />
+            </div>
+            <h1 className='text-6xl p-8'>Alex Garcia</h1>
+          {/* <div className='col'>
+            <Image
+              src={profilePic}
+              width={500}
+              height={500}
+              placeholder='blur'
+            />
+          </div> */}
         </div>
       </header>
 
