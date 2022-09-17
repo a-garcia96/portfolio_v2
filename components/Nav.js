51b7b59775f33bdac0237a706b73795df5828772
a-garcia96 <alex.g9415@gmail.com>
2022-09-16 22:29:52 -0700

    desktop view

--- components/Nav.js
+++ components/Nav.js
@@ -5,12 +5,12 @@ import Link from 'next/Link'
 export default function Nav() {
     return (
         <nav className='p-8'>
-            <div className='flex flex-col md:container md:mx-auto '>
+            <div className='flex flex-col md:container md:mx-auto md:flex md:flex-row'>
                 <div className='w-full md:w-3/6'>
-                    <h1 className='text-xl text-center fs'>LOGO</h1>
+                    <h1 className='text-xl text-center md:text-start'>LOGO</h1>
                 </div>
                 <div className='w-full md:w-3/6'>
-                    <ul className='flex flex-row justify-center mt-8 space-x-4'>
+                    <ul className='flex flex-row justify-center mt-8 space-x-4 md:space-x-8 md:mt-0 md:justify-end'>
                         <li>
                             <Link href='/'>
                                 <a>Home</a>
