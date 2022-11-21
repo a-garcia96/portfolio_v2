ff909010dc4eda63e98ea5857ae455e723da4a73
a-garcia96 <alex.g9415@gmail.com>
2022-11-21 14:33:33 -0800

    refactor

--- components/Nav.js
+++ components/Nav.js
@@ -1,39 +1,60 @@
-import React from 'react'
-import Link from 'next/Link'
-
+import React from "react";
+import Link from "next/Link";
 
 export default function Nav() {
-    return (
-        <nav className='p-8'>
-            <div className='flex flex-col md:container md:mx-auto md:flex md:flex-row'>
-                <div className='w-full md:w-3/6'>
-                    <h1 className='text-xl text-center md:text-start'>LOGO</h1>
-                </div>
-                <div className='w-full md:w-3/6'>
-                    <ul className='flex flex-row justify-center mt-8 space-x-4 md:space-x-8 md:mt-0 md:justify-end'>
-                        <li>
-                            <Link href='/'>
-                                <a>Home</a>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href='/portfolio'>
-                                <a>Portfolio</a>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href='/blog'>
-                                <a>Blog</a>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href='/contact'>
-                                <a>Contact</a>
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    )
+  return (
+    <nav>
+      <div className="container">
+        <div>
+          <h1>LOGO</h1>
+        </div>
+        <div>
+          <ul className="main-nav">
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/portfolio">
+                <a>Portfolio</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog">
+                <a>Blog</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact">
+                <a>Contact</a>
+              </Link>
+            </li>
+          </ul>
+          <ul className="mobile-nav">
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/portfolio">
+                <a>Portfolio</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog">
+                <a>Blog</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact">
+                <a>Contact</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
