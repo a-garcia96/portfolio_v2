2172b858f28532c07382c5c769335d6065748c3d
a-garcia96 <alex.g9415@gmail.com>
2022-12-01 11:57:17 -0800

    allow domain for images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'place-hold.it',
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
