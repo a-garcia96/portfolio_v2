b7d879bb99ffd6f85ac387797864c352209055a2
a-garcia96 <alex.g9415@gmail.com>
2022-12-02 13:17:05 -0800

    mobile-nav functionality and styling

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,9 +1,27 @@
 import React, { useState } from "react";
 import Link from "next/Link";
 
-import styles from './nav.module.css'
-
 export default function Nav() {
+
+  const handleClick = () => {
+
+    const mobileNav = document.querySelector('.nav__mobile')
+    const navToggle = document.querySelector('.nav__toggle')
+
+    const visibility = mobileNav.getAttribute('data-visible')
+
+    if(visibility == "false") {
+      mobileNav.setAttribute('data-visible', true);
+      navToggle.setAttribute('data-toggled', true);
+      
+    } else if (visibility == "true") {
+      navToggle.setAttribute('data-toggled', false);
+      mobileNav.setAttribute('data-visible', false);
+    }
+
+    console.log(visibility);
+  }
+
 
   return (
     <nav className="nav">
@@ -11,30 +29,33 @@ export default function Nav() {
         <div>
           <h1 className="uppercase-text">Alex Garcia</h1>
         </div>
-        <div>
-          <ul className="nav__mobile">
+        <button onClick={handleClick} className="nav__toggle" data-toggles="false"></button>
+        <div data-visible="false" className="nav__mobile">
+          <ul>
             <li>
               <Link href="/">
-                <a>Home</a>
+                <a><span>00</span> Home</a>
               </Link>
             </li>
             <li>
               <Link href="/portfolio">
-                <a>Portfolio</a>
+                <a><span>01</span> Portfolio</a>
               </Link>
             </li>
             <li>
               <Link href="/blog">
-                <a>Blog</a>
+                <a><span>02</span> Blog</a>
               </Link>
             </li>
             <li>
               <Link href="/contact">
-                <a>Contact</a>
+                <a><span>03</span> Contact</a>
               </Link>
             </li>
           </ul>
-          <ul className="nav__desktop">
+          </div>
+          <div className="nav__desktop">
+          <ul>
             <li>
               <Link href="/">
                 <a>Home</a>
