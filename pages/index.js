0484809773a2b9d3c77f8ffb973c7364f71692e0
a-garcia96 <alex.g9415@gmail.com>
2022-12-02 10:38:46 -0800

    refactor skills grid

--- pages/index.js
+++ pages/index.js
@@ -26,135 +26,164 @@ export default function Home() {
       <Head>
         <title>Alex Garcia | Web Developer</title>
         <meta name="description" content="Built with NextJs" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
       </Head>
       <Nav />
-      <header>
+      <header className="home-header">
         <div className="container">
-          <div className="header-grid">
-            <section className="header-col-1">
-              <h1 className="content-margin-bottom">
-                Developer & Photographer.
-              </h1>
+          <div className="home-header__grid">
+            <section className="home-header__col-1">
+              <h1>Developer & Photographer.</h1>
             </section>
-            <section className="header-col-2">
-              <div className="header-image-wrapper">
+            <section className="home-header__col-1">
+              <div className="home-header__image-wrapper">
                 <Image
                   src={profilePic}
                   objectFit="contain"
-                  className="profile-pic"
+                  className="home-header__profile-pic"
                 />
               </div>
             </section>
           </div>
         </div>
       </header>
+
       <main className="main-content">
         <div className="container">
-          <section className="main-content-about-section">
-            <div className="about-col-1">
-              <h2 className="uppercase-text"> About Me</h2>
-            </div>
-            <div className="about-col-2">
-              <p>
-                I am a self-taught front-end developer based in San Diego,
-                California. For the last 4 years I have worked as a systems
-                engineer and administrator managing IT infrastructure and assets
-                for businesses in a multitude of industries. Recently I have
-                transitioned into a developer role for one of San Diego&aposs top
-                financial broker dealers. I have a passion for art and
-                technology that drives me as a developer to build products that
-                are both functional and visually appealing.
-              </p>
+          <section className="home-about">
+            <div className="home-about__grid">
+              <div className="home-about__col-1">
+                <h2> About Me</h2>
+              </div>
+              ¬
+              <div className="home-about__col-2">
+                <p>
+                  I am a self-taught front-end developer based in San Diego,
+                  California. For the last 4 years I have worked as a systems
+                  engineer and administrator managing IT infrastructure and
+                  assets for businesses in a multitude of industries. Recently I
+                  have transitioned into a developer role for one of San
+                  Diego&aposs top financial broker dealers. I have a passion for
+                  art and technology that drives me as a developer to build
+                  products that are both functional and visually appealing.
+                </p>
+              </div>
             </div>
           </section>
-          <section className="main-content-skills-section">
-            <h2 className="uppercase-text content-margin-bottom">Skills</h2>
-            <div className="skills-grid">
-              <Image
-                alt="icon"
-                src={htmlIcon}
-                objectFit="contain"
-                width={100}
-                height={100}
-              />
-              <Image
-                alt="icon"
-                src={cssIcon}
-                objectFit="contain"
-                width={100}
-                height={100}
-              />
-              <Image
-                alt="icon"
-                src={jsIcon}
-                objectFit="contain"
-                width={100}
-                height={100}
-              />
-              <Image
-                alt="icon"
-                src={reactIcon}
-                objectFit="contain"
-                width={100}
-                height={100}
-              />
-              <Image
-                alt="icon"
-                src={nodeIcon}
-                objectFit="contain"
-                width={100}
-                height={100}
-              />
-              <Image
-                alt="icon"
-                src={npmIcon}
-                objectFit="contain"
-                width={100}
-                height={100}
-              />
-              <Image
-                alt="icon"
-                src={sassIcon}
-                objectFit="contain"
-                width={100}
-                height={100}
-              />
-              <Image
-                alt="icon"
-                src={bootstrapIcon}
-                objectFit="contain"
-                width={100}
-                height={100}
-              />
-              <Image
-                alt="icon"
-                src={firebaseIcon}
-                objectFit="contain"
-                width={100}
-                height={100}
-              />
-              <Image
-                alt="icon"
-                src={contentfulIcon}
-                objectFit="contain"
-                width={100}
-                height={100}
-              />
-              <Image
-                alt="icon"
-                src={wordpressIcon}
-                objectFit="contain"
-                width={100}
-                height={100}
-              />
-              <Image
-                alt="icon"
-                src={nextjsIcon}
-                objectFit="contain"
-                width={100}
-                height={100}
-              />
+          <section className="home-skills">
+            <h2>Skills</h2>
+            <div className="home-skills__grid">
+              <div className="home-skills__col">
+                <Image
+                  alt="icon"
+                  src={htmlIcon}
+                  objectFit="contain"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className="home-skills__col">
+                <Image
+                  alt="icon"
+                  src={cssIcon}
+                  objectFit="contain"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className="home-skills__col">
+                <Image
+                  alt="icon"
+                  src={jsIcon}
+                  objectFit="contain"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className="home-skills__col">
+                <Image
+                  alt="icon"
+                  src={reactIcon}
+                  objectFit="contain"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className="home-skills__col">
+                <Image
+                  alt="icon"
+                  src={nodeIcon}
+                  objectFit="contain"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className="home-skills__col">
+                <Image
+                  alt="icon"
+                  src={npmIcon}
+                  objectFit="contain"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className="home-skills__col">
+                <Image
+                  alt="icon"
+                  src={sassIcon}
+                  objectFit="contain"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className="home-skills__col">
+                <Image
+                  alt="icon"
+                  src={bootstrapIcon}
+                  objectFit="contain"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className="home-skills__col">
+                <Image
+                  alt="icon"
+                  src={firebaseIcon}
+                  objectFit="contain"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className="home-skills__col">
+                <Image
+                  alt="icon"
+                  src={contentfulIcon}
+                  objectFit="contain"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className="home-skills__col">
+                <Image
+                  alt="icon"
+                  src={wordpressIcon}
+                  objectFit="contain"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className="home-skills__col">
+                <Image
+                  alt="icon"
+                  src={nextjsIcon}
+                  objectFit="contain"
+                  width={100}
+                  height={100}
+                />
+              </div>
             </div>
           </section>
         </div>
