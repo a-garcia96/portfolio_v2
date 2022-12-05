2153fe42b79f99ae43ee576d5e41bfb9e4c5bd48
a-garcia96 <alex.g9415@gmail.com>
2022-12-05 13:38:06 -0800

    responsive design for ipad

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -2,83 +2,96 @@ import React, { useState } from "react";
 import Link from "next/Link";
 
 export default function Nav() {
-
   const handleClick = () => {
+    const mobileNav = document.querySelector(".nav__mobile");
+    const navToggle = document.querySelector(".nav__toggle");
 
-    const mobileNav = document.querySelector('.nav__mobile')
-    const navToggle = document.querySelector('.nav__toggle')
+    const visibility = mobileNav.getAttribute("data-visible");
 
-    const visibility = mobileNav.getAttribute('data-visible')
-
-    if(visibility == "false") {
-      mobileNav.setAttribute('data-visible', true);
-      navToggle.setAttribute('data-toggled', true);
-      document.body.style.overflowY = 'hidden'
-      
+    if (visibility == "false") {
+      mobileNav.setAttribute("data-visible", true);
+      navToggle.setAttribute("data-toggled", true);
+      document.body.style.overflowY = "hidden";
     } else if (visibility == "true") {
-      navToggle.setAttribute('data-toggled', false);
-      mobileNav.setAttribute('data-visible', false);
-      document.body.style.overflowY = 'visible'
+      navToggle.setAttribute("data-toggled", false);
+      mobileNav.setAttribute("data-visible", false);
+      document.body.style.overflowY = "visible";
     }
 
     console.log(visibility);
-  }
-
+  };
 
   return (
     <nav className="nav">
       <div className="container">
         <div className="nav__mobile-heading">
           <h1 className="uppercase-text">Alex Garcia</h1>
-          <button onClick={handleClick} className="nav__toggle" data-toggles="false"></button>
+          <button
+            onClick={handleClick}
+            className="nav__toggle"
+            data-toggles="false"
+          ></button>
         </div>
         <div data-visible="false" className="nav__mobile">
           <ul>
             <li>
               <Link href="/">
-                <a><span className="set-bold">00</span> Home</a>
+                <a>
+                  <span className="set-bold">00</span> Home
+                </a>
               </Link>
             </li>
             <li>
               <Link href="/portfolio">
-                <a><span className="set-bold">01</span> Portfolio</a>
+                <a>
+                  <span className="set-bold">01</span> Portfolio
+                </a>
               </Link>
             </li>
             <li>
               <Link href="/blog">
-                <a><span className="set-bold">02</span> Blog</a>
+                <a>
+                  <span className="set-bold">02</span> Blog
+                </a>
               </Link>
             </li>
             <li>
               <Link href="/contact">
-                <a><span className="set-bold">03</span> Contact</a>
+                <a>
+                  <span className="set-bold">03</span> Contact
+                </a>
               </Link>
             </li>
           </ul>
+        </div>
+        <div className="nav__desktop">
+          <div className="nav__desktop-heading">
+            <h1>Alex Garcia</h1>
           </div>
-          <div className="nav__desktop">
-          <ul>
-            <li>
-              <Link href="/">
-                <a>Home</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/portfolio">
-                <a>Portfolio</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/blog">
-                <a>Blog</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/contact">
-                <a>Contact</a>
-              </Link>
-            </li>
-          </ul>
+          <div className="nav__desktop-links">
+            <ul>
+              <li>
+                <Link href="/">
+                  <a>Home</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/portfolio">
+                  <a>Portfolio</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/blog">
+                  <a>Blog</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/contact">
+                  <a>Contact</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
