770197127a48af3dc5220379395c889632c5e20d
a-garcia96 <alex.g9415@gmail.com>
2022-12-15 10:35:51 -0800

    useContenful imported to fetch data from contentful

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 // IMPORT COMPONENTS
 import Nav from "../../components/Nav/Nav";
 import Footer from "../../components/Footer/Footer";
 
-const blog = () => {
+
+// IMPORT HOOKS
+import { useContentful } from "../../hooks/useContentful";
+
+export async function getStaticProps(context) {
+
+  const [ getEntryById ] = useContentful()
+
+  const entries =  await getEntryById('post');
+
+  const posts = entries.items.map((item) => {
+    return {
+      profilePhoto: item.fields.profilePhoto.fields.file.url,
+      title: item.fields.title,
+      author: item.fields.author.fields.displayName,
+      slug: item.fields.slug,
+      postDate: item.fields.postDate,
+      postContent: item.fields.postContent.content[0].content[0].value
+    }
+  })
+
+  
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+const blog = ({ posts }) => {
+  // console.log(JSON.stringify(entries.items[0].fields, null, 2))
+
+  
+
   return (
     <>
       <Nav />
@@ -15,12 +49,15 @@ const blog = () => {
       </header>
       <main>
         <div className="container">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat
-            qui, maiores quod quibusdam itaque dicta distinctio quo aliquam et
-            sed! Nihil iusto dignissimos, numquam perspiciatis eius labore sint
-            nulla ducimus.
-          </p>
+          {posts.map((post) => {
+            return (
+              <div key={post.slug}>
+                <h1>{post.title}</h1>
+                <h2>{post.author}</h2>
+                <p>{post.postContent.substring(0, 250) + '...'}</p>
+              </div>
+            )
+          })}
         </div>
       </main>
       <Footer />
