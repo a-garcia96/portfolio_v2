59359c70ebbf1f18f62bbd0863b5536dc5fbf067
a-garcia96 <alex.g9415@gmail.com>
2022-12-21 21:14:21 -0800

    displaying resume on /resume page

--- pages/resume.js
+++ pages/resume.js
@@ -1,20 +1,29 @@
-import React, { useState } from 'react'
+import Head from "next/head";
+import React from "react";
 
-import { Document, Page } from 'react-pdf'
+import { pdfjs, Document, Page } from "react-pdf";
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 
+import Nav from "../components/Nav/Nav";
+import Footer from "../components/Footer/Footer";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const Resume = () => {
-
-    const pdf = './myResume2023.pdf'
-
-    return (
-        <div>
-            <h1>Resume!</h1>
-            <Document file={pdf}>
-                <Page pageNumber={1} />
-            </Document>
+  return (
+    <>
+      <Head></Head>
+      <Nav />
+      <main>
+        <div className="container">
+          <Document className="resume" file={"/myResume2023.pdf"}>
+            <Page pageIndex={0} />
+          </Document>
         </div>
-    )
-}
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export default Resume
+export default Resume;
