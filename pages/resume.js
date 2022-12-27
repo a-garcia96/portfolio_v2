9cc043cdfe9f8a2fc656e6dd4ea412648af51c35
Alex Garcia <alex.g9415@gmail.com>
2022-12-27 13:21:16 -0800

    change markup

--- pages/resume.js
+++ pages/resume.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import React from "react";
+import Image from "next/image";
 
 import { pdfjs, Document, Page } from "react-pdf";
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
@@ -7,6 +8,7 @@ import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 import Nav from "../components/Nav/Nav";
 import Footer from "../components/Footer/Footer";
+import resume from '../public/myResume2023.png'
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
@@ -16,10 +18,15 @@ const Resume = () => {
       <Head></Head>
       <Nav />
       <main>
-        <div className="container">
-          <Document className="resume" file={"/myResume2023.pdf"}>
-            <Page pageIndex={0}/>
-          </Document>
+        <div className="container resume">
+          <div className="resume__grid">
+            <div className="resume__column-1">
+              <Image style={{maxWidth: "350px !important"}} src={resume} className="resume__image" />
+            </div>
+            <div className="resume__column-2">
+              <h2>My Work Experience</h2>
+            </div>
+          </div>
         </div>
       </main>
       <Footer />
