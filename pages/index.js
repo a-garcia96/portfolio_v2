7ee2012fad10de60c2857dd5a1b411846ae98740
Alex Garcia <alex.g9415@gmail.com>
2022-12-27 13:03:51 -0800

    added styling to home page

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Image from "next/image";
 import Nav from "../components/Nav/Nav";
 import Button from "../components/Button/Button";
 import Footer from "../components/Footer/Footer";
-import {Typewriter} from 'react-simple-typewriter';
+import { Typewriter } from 'react-simple-typewriter';
 
 // assets
 import profilePic from "../public/profilePic2.jpg";
@@ -32,7 +32,7 @@ export default function Home() {
     const handleResize = () => {
       if (window.innerWidth < 450) {
         setDimension(60);
-      } else if(window.innerWidth > 450) {
+      } else if (window.innerWidth > 450) {
         setDimension(80);
       }
 
@@ -57,7 +57,7 @@ export default function Home() {
         <div className="container">
           <div className="home-header__grid">
             <section className="home-header__col-1">
-              <h1><Typewriter cursor={true} typeSpeed={120} words={['developing','marketing','optimizing']} loop={0}/> <br /><span className="highlight-text">for the web.</span></h1>
+              <h1><Typewriter cursor={true} typeSpeed={120} words={['developing', 'marketing', 'optimizing']} loop={0} /> <br /><span className="highlight-text">for the web.</span></h1>
               {/* <h2 className="highlight-text">for the web.</h2> */}
               <p>Self-taught front-end developer based in San Diego, California with a background in IT cloud infrastructure engineering and administration.</p>
               <Button><Link href="/resume">See My Resume</Link></Button>
@@ -75,8 +75,8 @@ export default function Home() {
       </header>
 
       <main className="main-content">
-        <div className="container">
-          <section className="home-about">
+        <section className="home-about">
+          <div className="container">
             <div className="home-about__grid">
               <div className="home-about__col-1">
                 <h2>About Me</h2>
@@ -86,15 +86,17 @@ export default function Home() {
                   For the last 4 years I have worked as a systems
                   engineer and administrator managing IT infrastructure and
                   assets for businesses in a multitude of industries. Recently I
-                  have transitioned into a developer role for one of San
+                  have transitioned to a developer role for one of San
                   Diego&#39;s top financial broker dealers. I have a passion for
                   art and technology that drives me as a developer to build
                   products that are equally functional as they are visually appealing.
                 </p>
               </div>
             </div>
-          </section>
-          <section className="home-skills">
+          </div>
+        </section>
+        <section className="home-skills">
+          <div className="container">
             <h2>Tech Stack</h2>
             <div className="home-skills__grid">
               <div className="home-skills__col">
@@ -205,8 +207,8 @@ export default function Home() {
                 />
               </div>
             </div>
-          </section>
-        </div>
+          </div>
+        </section>
       </main>
       <Footer />
     </>
