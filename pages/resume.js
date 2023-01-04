703754b584d023fff5e252c9ad559da6e123c08b
a-garcia96 <alex.g9415@gmail.com>
2023-01-03 21:36:49 -0800

    updates to resume pagea

--- pages/resume.js
+++ pages/resume.js
@@ -2,15 +2,10 @@ import Head from "next/head";
 import React from "react";
 import Image from "next/image";
 
-import { pdfjs, Document, Page } from "react-pdf";
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
-
 import Nav from "../components/Nav/Nav";
 import Footer from "../components/Footer/Footer";
-import resume from '../public/myResume2023.png'
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import resume from "../public/myResume2023.png";
+import Button from "../components/Button/Button";
 
 const Resume = () => {
   return (
@@ -21,10 +16,17 @@ const Resume = () => {
         <div className="container resume">
           <div className="resume__grid">
             <div className="resume__column-1">
-              <Image style={{maxWidth: "350px !important"}} src={resume} className="resume__image" />
-            </div>
-            <div className="resume__column-2">
-              <h2>My Work Experience</h2>
+              <h2 className="resume__heading">My Work Experience</h2>
+              <Image
+                style={{ maxWidth: "350px !important" }}
+                src={resume}
+                className="resume__image"
+              />
+              <Button className="resume__download-button">
+                <a style={{padding: "2em"}} href="https://drive.google.com/file/d/1Ye7c0GpkskZ_53lTyYhCPhMpEK0d7c9o/view?usp=sharing" download="Alex Garcia Resume">
+                  Download the pdf
+                </a>
+              </Button>
             </div>
           </div>
         </div>
