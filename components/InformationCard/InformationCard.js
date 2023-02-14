dbe5da59dc97d3cbaeb09230cccf8a1e86ee3e8a
Alex Garcia <alex.g9415@gmail.com>
2023-02-14 07:55:34 -0800

    new component

---
+++ components/InformationCard/InformationCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const InformationCard = ({heading, summaryText}) => {
+  return (
+    <div className='information-card'>
+        {/* ICON or Image */}
+        {/* Heading */}
+        {/* Summary Text */}
+        {/* CTA (IF NEEDED) */}
+        <img src='' alt=''/>
+        <h3>{heading}</h3>
+        <p>{summaryText}</p>
+    </div>
+  )
+}
+
+export default InformationCard
