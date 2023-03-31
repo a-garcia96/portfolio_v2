1b82e7dd0c2a7ff38c7122084e83ddbeb2fab57d
Alex Garcia <alex.g9415@gmail.com>
2023-03-31 13:43:38 -0700

    removed avatar

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -5,14 +5,6 @@ import Link from "next/Link";
 const Card = ({ post }) => {
   return (
     <article className="card">
-      <div className="card__avatar">
-        <img
-          className="card__avatar-image"
-          src={post.profilePhoto}
-          alt="Photo of the author"
-        />
-        <span><h3 className="card__author">{post.author}</h3></span>
-      </div>
       <h2 className="card__title">{post.title}</h2>
       <p className="card__post-date">{post.postDate}</p>
       <div className="card__details">
