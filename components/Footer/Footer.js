c88d72238443c047f534b74a4c8eb88b7b16df58
Alex Garcia <alex.g9415@gmail.com>
2023-05-08 08:47:01 -0700

    styling

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,20 +1,32 @@
 import React from 'react'
 import Link from "next/link"
+import Image from 'next/image';
+
+import linkedinIcon from "../../public/linkedin-icon.png";
+import instagramIcon from "../../public/instagram-icon.png"
 
 const Footer = () => {
   return (
     <footer className='footer'>
-        <div className='footer__wrapper container'>
-          <div className='footer__site-map'>
-            <ul>
-              <li><Link href="/">Home</Link></li>
-              <li><Link href="/portfolio">Portfolio</Link></li>
-              <li><Link href="/blog">Blog</Link></li>
-              <li><Link href="/about">About</Link></li>
-            </ul>
+      <div className='footer__wrapper container'>
+        <div className='footer__dev-profile'>
+          <h3>Alex Garcia</h3>
+          <a href="mailto:alex.g9415@gmail.com?subject=email from your portfolio">alex.g9415@gmail.com</a>
+          <div className='footer__social-wrapper'>
+            <a href="https://linkedin.com/in/agarcia96"><Image src={linkedinIcon} width={30} height={30} /></a>
+            <a href="https://www.instagram.com/alex_gaarciaa/"><Image src={instagramIcon} width={30} height={30} /></a>
           </div>
-          <div className='footer__copyright'>©{new Date().getFullYear()} | Alex Garcia</div>
         </div>
+        <div className='footer__site-map'>
+          <ul>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/portfolio">Portfolio</Link></li>
+            <li><Link href="/blog">Blog</Link></li>
+            <li><Link href="/about">About</Link></li>
+          </ul>
+        </div>
+      </div>
+      <div className='footer__copyright'>©{new Date().getFullYear()}</div>
     </footer>
   )
 }
