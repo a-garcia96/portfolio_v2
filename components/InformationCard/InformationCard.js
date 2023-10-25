8a270d5d12380b10cf015edc69dce3775169e8a7
Alex Garcia <alex.g9415@gmail.com>
2023-10-24 20:44:08 -0700

    remove old image references

--- components/InformationCard/InformationCard.js
+++ components/InformationCard/InformationCard.js
@@ -4,10 +4,6 @@ import Image from 'next/image'
 const InformationCard = ({imageSrc, heading, summaryText}) => {
   return (
     <div className='information-card'>
-        {/* ICON or Image */}
-        {/* Heading */}
-        {/* Summary Text */}
-        {/* CTA (IF NEEDED) */}
         <Image src={imageSrc}/>
         <h3 className='information-card__heading'>{heading}</h3>
         <p className='information-card__text'>{summaryText}</p>
