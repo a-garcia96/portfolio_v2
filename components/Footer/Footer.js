b5a7d3e62b404c375fff04592f345ca289ed2db9
Alex Garcia <alex.g9415@gmail.com>
2023-10-24 20:43:54 -0700

    change styling for new design

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,34 +1,26 @@
-import React from 'react'
-import Link from "next/link"
-import Image from 'next/image';
+import React from "react";
 
-import linkedinIcon from "../../public/linkedin-icon.png";
-import instagramIcon from "../../public/instagram-icon.png"
+import Container from "../Container/Container";
+
+{
+  /* <div className='footer__copyright'></div> */
+}
 
 const Footer = () => {
   return (
-    <footer className='footer'>
-      <div className='footer__wrapper container'>
-        <div className='footer__dev-profile'>
-          <h3>Alex Garcia</h3>
-          <a href="mailto:alex.g9415@gmail.com?subject=email from your portfolio">alex.g9415@gmail.com</a>
-          <div className='footer__social-wrapper'>
-            <a href="https://linkedin.com/in/agarcia96"><Image src={linkedinIcon} width={30} height={30} /></a>
-            <a href="https://www.instagram.com/alex_gaarciaa/"><Image src={instagramIcon} width={30} height={30} /></a>
+    <Container classes={"my-8"}>
+      <footer className="bg-white rounded-full py-1 px-2 shadow-sm">
+        <div className="py-2 px-4 grid grid-cols-3 text-sm">
+          <div className="flex gap-1 col-span-2">
+            <p>Developed by Alex Garcia</p>
+            <p>|</p>
+            <p className="font-bold flex gap-1">Source Code <svg className="w-5" viewBox="0 0 24 24" class="fill-white dark:fill-black"><path d="M12,2A10,10,0,0,0,8.84,21.5c.5.08.66-.23.66-.5V19.31C6.73,19.91,6.14,18,6.14,18A2.69,2.69,0,0,0,5,16.5c-.91-.62.07-.6.07-.6a2.1,2.1,0,0,1,1.53,1,2.15,2.15,0,0,0,2.91.83,2.16,2.16,0,0,1,.63-1.34C8,16.17,5.62,15.31,5.62,11.5a3.87,3.87,0,0,1,1-2.71,3.58,3.58,0,0,1,.1-2.64s.84-.27,2.75,1a9.63,9.63,0,0,1,5,0c1.91-1.29,2.75-1,2.75-1a3.58,3.58,0,0,1,.1,2.64,3.87,3.87,0,0,1,1,2.71c0,3.82-2.34,4.66-4.57,4.91a2.39,2.39,0,0,1,.69,1.85V21c0,.27.16.59.67.5A10,10,0,0,0,12,2Z"></path></svg></p>
           </div>
+          <div className="col-span-1 justify-self-end"><p>Alex Garcia © {new Date().getFullYear()}</p></div>
         </div>
-        <div className='footer__site-map'>
-          <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/portfolio">Portfolio</Link></li>
-            <li><Link href="/blog">Blog</Link></li>
-            <li><Link href="/about">About</Link></li>
-          </ul>
-        </div>
-      </div>
-      <div className='footer__copyright'>©{new Date().getFullYear()}</div>
-    </footer>
-  )
-}
+      </footer>
+    </Container>
+  );
+};
 
-export default Footer
+export default Footer;
