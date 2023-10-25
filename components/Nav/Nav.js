3bdecc9e009a1012aae9f68d00218fc7ee9183a7
Alex Garcia <alex.g9415@gmail.com>
2023-10-24 20:44:53 -0700

    remove reference to deleted image file

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import nameLogo from "../../public/name_logo.png"
 
 // COMPONENTS
 import Container from "../Container/Container";
