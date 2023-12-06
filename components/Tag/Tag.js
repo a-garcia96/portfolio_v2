bc52a15d2a0a29b98f5d560b6f877430426d3dff
Alex Garcia <alex.g9415@gmail.com>
2023-12-05 21:16:13 -0800

    update text color

--- components/Tag/Tag.js
+++ components/Tag/Tag.js
@@ -4,7 +4,7 @@ const Tag = ({ tag }) => {
   return (
     <li
       key={tag}
-      className="bg-neutral-100 rounded-full px-4 py-2 text-xs w-fit"
+      className="bg-neutral-100 rounded-full px-4 py-2 text-xs w-fit text-blue-500 font-bold"
     >
       {tag}
     </li>
